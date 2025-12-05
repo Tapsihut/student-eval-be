fb3052345909eb03f8974c5a994f99ae662b743d
Wybie <lyrncofyn@gmail.com>
2025-12-05 22:33:46 +0800

    fix: correct comment for fetching curriculum subjects

--- app/services/AdvisingService.php
+++ app/services/AdvisingService.php
@@ -16,7 +16,7 @@ class AdvisingService
      */
     public function generateAdvising($curriculum, Collection $ocrRecords): array
     {
-        // 🧾 Get all curriculum subjects
+        // 🧾 Get all curriculum subject
         $subjects = Subject::where('curriculum_id', $curriculum->id)
             ->with('prerequisites')
             ->get();
